8295134ea9f07d7d349f84471457a4407bb52119
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-07-25 15:23:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function RamdomIndex(array) {
+    let ArrayIndex = Math.floor(Math.random() * array.length);
+    return array[ArrayIndex];
+  }
+
+  let pinta = ["♤", "♡", "♧", "♢"];
+  let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let pintaCarta = RamdomIndex(pinta);
+  let numeroCarta = RamdomIndex(number);
+  let elementpintaCarta = document.querySelector("#pintaA");
+  let elementpintaCartaAb = document.querySelector("#pintaAb");
+  let elementnumeroCarta = document.querySelector("#numberCard");
+
+  elementpintaCarta.innerHTML = pintaCarta;
+  elementpintaCartaAb.innerHTML = pintaCarta;
+  elementnumeroCarta.innerHTML = numeroCarta;
+
+  elementpintaCartaAb.style.transform = "rotate(180deg)";
+
+  if (pintaCarta == "♡" || pintaCarta == "♢") {
+    elementpintaCarta.style.color = "red";
+    elementpintaCartaAb.style.color = "red";
+  }
 };
